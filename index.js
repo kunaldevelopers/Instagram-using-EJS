ce4031219a2a6b5e877a53095526e88ca9e63baa
kunaldevelopers <kunalking01grd@gmail.com>
2024-06-06 15:38:45 +0530

    Making Some Changesgit add .

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const ejs = require("ejs");
 let port = 8080;
 
+app.use(express.static(path.join(__dirname,"public/CSS")));
+app.use(express.static(path.join(__dirname, "/public/JS")));
+
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/in/:username", (req, res)=>{
     let {username} = req.params;
-    res.render("insta.ejs", {username});
-    console.log(`Username is : ${username}`);
+    let instaData = require("./data.json");
+    const data = instaData[username];
+    if(data){
+        res.render("insta.ejs", {data});
+    }else{
+        res.render("error.ejs");
+    }
 })
 
 app.listen(port, (req, res)=>{
     console.log(`Port is Listening at ${port}`);
 });
+
+
